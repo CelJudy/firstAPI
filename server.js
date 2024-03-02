01683e766f215b692b12e9c50ab036c0b1a6b39b
CelJudy <celjudygmail.com>
2024-03-02 12:52:55 -0600

    index

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const app = express();
 app.get("/", (req, res) => {
     //res.send("holi");
     //console.log(JSON.parse("{'body':{'clave':'prueba'}}"));
+    res.sendFile(__dirname+"/index.html");
+});
+
+app.get("/api", (req, res) => {
+    //res.send("holi");
+    //console.log(JSON.parse("{'body':{'clave':'prueba'}}"));
     res.json({body:{clave:'prueba'}})
 });
 
